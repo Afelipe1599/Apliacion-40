53ce384a2c844870c8fd1b77245e3eaf3981066a
Luis Restrepo <luis.david.restrepo@correounivalle.edu.co>
2020-10-18 21:06:34 -0500

    hamburguesa que no funciona bien y layout de reportes

--- ejerciciosmas40/app/src/main/java/com/example/ejerciciosmas40/TestActivity.java
+++ ejerciciosmas40/app/src/main/java/com/example/ejerciciosmas40/TestActivity.java
@@ -1,14 +1,40 @@
 package com.example.ejerciciosmas40;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class TestActivity extends AppCompatActivity {
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class TestActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_abierto, R.string.nav_cerrado);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        return true;
     }
 }
